1f459f1d551c21c0dc87a57e860e141626454b72
xijieyin <xijieyin@gmail.com>
2022-07-10 21:31:14 +0800

    fix: twitter share

--- components/buttons/ShareButton.tsx
+++ components/buttons/ShareButton.tsx
@@ -1,18 +1,18 @@
 import { Profile } from "types";
 import queryString from "query-string";
 import Button from "./Buttons";
-import SvgIcon from "components/icons/SvgIcon";
 
 const ShareButton = ({ profile }: { profile: Profile }) => {
   const share = () => {
     let handle = profile.handle;
+    const twitter = profile.attributes.find((a) => a.key === "twitter")?.value;
     if (handle === "lensprotocol") handle += ".lens";
     const shareUrl =
       "https://twitter.com/share?" +
       queryString.stringify({
         url: `https://${handle}.ink`,
-        text: profile.name,
-        via: "lens.ink",
+        text: `${profile.name}  ${twitter ? "@" : ""}${twitter}`,
+        via: "_lensink",
       });
     return window.open(shareUrl, "Share to Twitter");
   };
