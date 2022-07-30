79143a548ec43bd38bf2861ca07133aa5361d51b
xijieyin <xijieyin@gmail.com>
2022-07-30 19:09:48 +0800

    test: rename dir

--- tests/homepage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-});
