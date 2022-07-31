0379e073e8f47cf19d7be759d507ab9bd856b5e2
xijieyin <xijieyin@gmail.com>
2022-07-31 14:14:18 +0800

    ci: fix test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { type PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  testDir: 'e2e',
   use: {
     trace: 'on-first-retry',
   },
